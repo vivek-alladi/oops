ce000fd5667c121d8cea7d4390d1edb83fb59ab8
vivek-alladi <vivek.alladi26@gmail.com>
2023-04-25 00:29:57 +0530

    Add. interface example defining apis and abstraction

---
+++ src/main/java/Interface/Abstraction/Main.java
@@ -0,0 +1,7 @@
+package Interface.Abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Look for abstraction example");
+    }
+}
---
+++ src/main/java/Interface/DefiningAPI/Main.java
@@ -0,0 +1,7 @@
+package Interface.DefiningAPI;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Check Polymorphsm - with interface example");
+    }
+}
