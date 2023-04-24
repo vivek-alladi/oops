3af5ffdfb8027bd845b55908e1d6c26d7bb04e89
vivek-alladi <vivek.alladi26@gmail.com>
2023-04-24 23:50:19 +0530

    Add. abstraction using interface

---
+++ src/main/java/Abstraction/Interface/Main.java
@@ -0,0 +1,46 @@
+package Abstraction.Interface;
+
+
+interface Shape {
+    double getArea();
+}
+
+class Circle implements Shape {
+    protected double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public double getArea(){
+        return Math.PI*radius*radius;
+    }
+}
+
+class Rectangle implements Shape {
+    private double width;
+    private double height;
+
+    public Rectangle(double width, double height) {
+        this.height = height;
+        this.width =width;
+    }
+
+    public double getArea() {
+        return width* height;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle(5.0);
+        Shape rectangle = new Rectangle(2.0, 3.0);
+
+        double circleArea = circle.getArea();
+        double rectangleArea = rectangle.getArea();
+        System.out.println(circleArea);
+        System.out.println(rectangleArea);
+
+    }
+}
